f29bc099645042f40f0c08bd8a6f9d6184f11b9d
Ankita <ankita.kumari@tesco.com>
2019-01-29 19:34:18 +0530

    Checking prettier

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,47 +1,51 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import classes from './Person.css';
-import WithClass from '../../../hoc/WithClass';
+import classes from "./Person.css";
+import WithClass from "../../../hoc/WithClass";
 
 class Person extends Component {
-    constructor(props){
-        console.log('[Person.js]inside constructor',props);
-        super(props);
-        this.inputElement = React.createRef();
-    }
+  constructor(props) {
+    console.log("[Person.js]inside constructor", props);
+    super(props);
+    this.inputElement = React.createRef();
+  }
 
-      componentWillMount(){
-        console.log('[Person.js]inside componentWillMount');
-      }
-      componentDidMount(){
-        console.log('[Person.js]inside componentDidMount');
-        if(this.props.position === 0){
-            this.inputElement.current.focus();
-        }
-      }
-      componentWillReceiveProps(nextProps){
-        console.log('[UPDATE Person.js]inside componenWillReceiveProps', nextProps);
-      }
-    render(){
-        console.log('[Person.js]inside render');
-        return <WithClass className={classes.Person}>
-                    { this.props.authenticated ? <p>I'm authenticated</p> : null }
-                    <p onClick = { this.props.click }>I am { this.props.name }! My age is { this.props.age }</p>
-                    <input 
-                        ref={this.inputElement}
-                        type='text'
-                        onChange= {this.props.changed}
-                        value={this.props.name}>
-                    </input>
-               </WithClass>
+  componentWillMount() {
+    console.log("[Person.js]inside componentWillMount");
+  }
+  componentDidMount() {
+    console.log("[Person.js]inside componentDidMount");
+    if (this.props.position === 0) {
+      this.inputElement.current.focus();
     }
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log("[UPDATE Person.js]inside componenWillReceiveProps", nextProps);
+  }
+  render() {
+    console.log("[Person.js]inside render");
+    return (
+      <WithClass className={classes.Person}>
+        {this.props.authenticated ? <p>I'm authenticated</p> : null}
+        <p onClick={this.props.click}>
+          I am {this.props.name}! My age is {this.props.age}
+        </p>
+        <input
+          ref={this.inputElement}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </WithClass>
+    );
+  }
 }
 Person.propTypes = {
-    click: PropTypes.func,
-    name: PropTypes.string,
-    age: PropTypes.number,
-    changed: PropTypes.func,
-}
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default Person;
